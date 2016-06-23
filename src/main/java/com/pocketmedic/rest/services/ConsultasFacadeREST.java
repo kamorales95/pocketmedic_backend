5a9a9edb247f349c497b3076da8d4ec2d1294d4e
Emerson Caicedo <ecaicedo242@misena.edu.co>
2016-06-23 11:17:33 -0500

    services de consultas

--- src/main/java/com/pocketmedic/rest/services/ConsultasFacadeREST.java
+++ src/main/java/com/pocketmedic/rest/services/ConsultasFacadeREST.java
@@ -5,11 +5,18 @@
  */
 package com.pocketmedic.rest.services;
 
+import com.nimbusds.jose.JOSEException;
 import com.pocketmedic.jpa.entities.Consulta;
 import com.pocketmedic.jpa.entities.Usuario;
 import com.pocketmedic.jpa.sessions.ConsultaFacade;
+import com.pocketmedic.rest.auth.AuthUtils;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,6 +25,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -29,18 +37,29 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 
 public class ConsultasFacadeREST {
+    
     @EJB
     
     ConsultaFacade consultaFacade;
     
+    @Context
+    private HttpServletRequest request;
+    
     @GET
-    public List<Consulta>findAll(){
+    public List<Consulta> findAll() {
         return consultaFacade.findAll();
     }
     
     @POST
-    public void create(Consulta consulta){
-        consultaFacade.create(consulta);
+    public void create(Consulta consulta) {
+        try {
+            consulta.setIdUsuario(new Usuario(Integer.parseInt(AuthUtils.getSubject(request.getHeader(AuthUtils.AUTH_HEADER_KEY)))));
+            consulta.setFechaCita(new Date());
+            consultaFacade.create(consulta);
+        } catch (ParseException | JOSEException ex) {
+            Logger.getLogger(ConsultasFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     @GET
@@ -51,20 +70,20 @@ public class ConsultasFacadeREST {
     
     @PUT
     @Path("{id}")
-    public void update(@PathParam("id") Integer id, Consulta consulta){
+    public void update(@PathParam("id") Integer id, Consulta consulta) {
         consultaFacade.edit(consulta);
     }
     
     @DELETE
     @Path("{id}")
-    public void delete (@PathParam("id") Integer id, Consulta consulta){
+    public void delete(@PathParam("id") Integer id, Consulta consulta) {
         consultaFacade.remove(consulta);
-    
+        
     }
     
     @GET
     @Path("usuario/{id}")
-    public List<Consulta> findByUsuario(@PathParam("id") Integer id){
+    public List<Consulta> findByUsuario(@PathParam("id") Integer id) {
         return consultaFacade.findByUsuario(new Usuario(id));
     }
     
